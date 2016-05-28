8afe50edc2d8b13366df114a23add7aa9a62fc90
Alex Korotkov <durm.icecoffee@gmail.com>
2016-05-28 14:01:46 +0300

    extends for contactor, emails, addresses

--- core/admin.py
+++ core/admin.py
@@ -1,16 +1,91 @@
 from django.contrib import admin
-from .models import Contractor, EmailToContractor
+from .models import Contractor, EmailToContractor, Address
 from django.forms import ModelForm
+from django.utils.translation import ugettext_lazy as _
 
-class ContractorAdmin(admin.ModelAdmin):
+
+class ContractorForm(ModelForm):
+    class Meta:
+        fields = '__all__'
+        model = Contractor
+
+    def __init__(self, *args, **kwargs):
+        super(ModelForm, self).__init__(*args, **kwargs)
+
+        """
+        self.fields['primary_email'].queryset = \
+            Contractor.objects.get(id=self.instance.id).emails.all() or \
+            EmailToContractor.objects.none()
+        self.fields['primary_address'].queryset = \
+            Contractor.objects.get(id=self.instance.id).addresses.all() or \
+            Address.objects.none()
+        """
+
+
+class PrototypeAdmin(admin.ModelAdmin):
+    readonly_fields = ("created_by", "created_at", "updated_by", "updated_at")
+
+    def save_model(self, request, obj, form, change):
+        if getattr(obj, 'id', None) is None:
+            obj.created_by = request.user
+        obj.updated_by = request.user
+        obj.save()
+
+
+class ContractorAdmin(PrototypeAdmin):
+    form = ContractorForm
+    readonly_fields = ("created_by", "created_at", "updated_by", "updated_at")
+
+    fieldsets = (
+        (
+            None,
+            {
+                "fields": (
+                    "name", "desc", "phone", "fax", "cite", "emails",
+                    "primary_email", "addresses", "primary_address",
+                )
+            }
+        ),
+        (
+            _("дополнительно"),
+            {
+                "fields": (
+                    "kind", "sector", "annual_income", "annual_income_currency",
+                    "employee_num", "sic", "stock_code", "own", "parent", "rating", "responsible"
+                )
+            }
+        ),
+        (
+            None,
+            {
+                "fields": (
+                    "created_by", "created_at", "updated_by", "updated_at"
+                )
+            }
+        )
+    )
+
+
+
+    filter_horizontal = ('emails', "addresses",)
+
+
+class AddressAdmin(PrototypeAdmin):
+    readonly_fields = ("created_by", "created_at", "updated_by", "updated_at")
+    fields = (
+        "desc", "zipcode", "country", "region", "city", "street",
+        "created_by", "created_at", "updated_by", "updated_at")
+
+
+class EmailToContractorAdmin(PrototypeAdmin):
+    #def get_model_perms(self, request):
+    #    """
+    #    Return empty perms dict thus hiding the model from admin index.
+    #    """
+    #    return {}
     pass
 
-class EmailToContractorAdmin(admin.ModelAdmin):
-    def get_model_perms(self, request):
-        """
-        Return empty perms dict thus hiding the model from admin index.
-        """
-        return {}
 
+admin.site.register(Address, AddressAdmin)
 admin.site.register(EmailToContractor, EmailToContractorAdmin)
 admin.site.register(Contractor, ContractorAdmin)
--- core/models.py
+++ core/models.py
@@ -1,69 +1,246 @@
 from django.db import models
-from django.contrib.auth.models import User
+from django.contrib.auth.models import User, Group
 from django.utils.translation import ugettext_lazy as _
 
-# timestamps
+
 class Prototype(models.Model):
     created_by = models.ForeignKey(
         User,
         null=False,
-        verbose_name=_("Создал"),
+        verbose_name=_("создал"),
         related_name="+"
     )
-    created_at = models.DateTimeField(auto_now_add=True, null=False, verbose_name=_("Время создания"))
+    created_at = models.DateTimeField(
+        auto_now_add=True,
+        null=False,
+        verbose_name=_("время создания"))
     updated_by = models.ForeignKey(
         User,
         null=False,
-        verbose_name=_("Изменил"),
+        verbose_name=_("изменил"),
         related_name="+"
     )
-    updated_at = models.DateTimeField(auto_now=True, null=False, verbose_name=_("Время изменения"))
+    updated_at = models.DateTimeField(
+        auto_now=True,
+        null=False,
+        verbose_name=_("время изменения")
+    )
 
     class Meta:
         abstract = True
 
+
 class Desc(models.Model):
-    desc = models.TextField(verbose_name=_("Описание"), blank=True)
+    desc = models.TextField(
+        verbose_name=_("описание"),
+        blank=True,
+        null=True
+    )
+
+    class Meta:
+        abstract = True
+
 
 class Named(Desc):
-    name = models.TextField(null=False, verbose_name=_("Наименование"))
+    name = models.TextField(
+        null=False,
+        verbose_name=_("наименование"),
+        blank=False
+    )
 
-#####
+    class Meta:
+        abstract = True
 
 class Address(Desc, Prototype):
-    latitude = models.FloatField(verbose_name=_("Широта"), blank=True)
-    longitude = models.FloatField(verbose_name=_("Долгота"), blank=True)
-
-    # manuals
-    street = models.TextField(verbose_name=_("Улица, дом/строение"), blank=True)
-    city = models.CharField(max_length=128, verbose_name=_("Город"), blank=True)
-    region = models.CharField(max_length=128, verbose_name=_("Область"), blank=True)
-    zipcode = models.IntegerField(verbose_name=_("Индекс"), blank=True)
-    country = models.CharField(max_length=128, verbose_name=_("Страна"), blank=True)
+    street = models.TextField(
+        verbose_name=_("улица, дом/строение"),
+        blank=True,
+        null=True
+    )
+    city = models.CharField(
+        max_length=128,
+        verbose_name=_("город"),
+        blank=False,
+        null=False,
+    )
+    region = models.CharField(
+        max_length=128,
+        verbose_name=_("область"),
+        blank=True,
+        null=True
+    )
+    zipcode = models.IntegerField(
+        verbose_name=_("индекс"),
+        blank=True,
+        null=True
+    )
+    country = models.CharField(
+        max_length=128,
+        verbose_name=_("страна"),
+        blank=True,
+        null=True
+    )
 
     def __str__(self):
-        return "{} {} {} {} {}".format(
-            self.zipcode or "",
-            self.country or "",
-            self.region or "",
-            self.city or "",
-            self.street
+        return ", ".join(
+            map(
+                str,
+                filter(
+                    None,
+                    (
+                    self.zipcode,
+                    self.country,
+                    self.region,
+                    self.city,
+                    self.street,
+                    )
+                )
+            )
         ).strip()
 
+    class Meta:
+        verbose_name = _("адрес")
+        verbose_name_plural = _("адреса")
+
+
 class EmailToContractor(Prototype):
-    email = models.EmailField(null=False, verbose_name=_("Email"))
-    noreply = models.BooleanField(null=False, default=False, verbose_name=_("Не писать"))
-    wrong = models.BooleanField(null=False, default=False, verbose_name=_("Неверный"))
+    email = models.EmailField(
+        null=False,
+        verbose_name=_("эл. адрес"),
+        blank=False
+    )
+    noreply = models.BooleanField(
+        null=False,
+        default=False,
+        verbose_name=_("не писать")
+    )
+    wrong = models.BooleanField(
+        null=False,
+        default=False,
+        verbose_name=_("неверный")
+    )
 
     def __str__(self):
         return self.email
 
+    class Meta:
+        verbose_name = _("эл. адрес")
+        verbose_name_plural = _("эл. адреса")
+
+
 class Contractor(Named, Prototype):
-    cite = models.URLField(verbose_name=_("Сайт"), blank=True)
-    phone = models.CharField(max_length=128, verbose_name=_("Телефон"))
-    fax = models.CharField(max_length=128, verbose_name=_("Факс"), blank=True)
-    emails = models.ManyToManyField(EmailToContractor, verbose_name=_("Emails"), related_name="+", blank=True)
-    email = models.ForeignKey(EmailToContractor, related_name="+")
- 
+    cite = models.URLField(
+        verbose_name=_("сайт"),
+        blank=True,
+        null=True
+    )
+    phone = models.CharField(
+        max_length=128,
+        verbose_name=_("телефон"),
+        null=False,
+        blank=False
+    )
+    fax = models.CharField(
+        max_length=128,
+        verbose_name=_("факс"),
+        blank=True,
+        null=True
+    )
+    emails = models.ManyToManyField(
+        EmailToContractor,
+        verbose_name=_("эл. адреса"),
+        related_name="+"
+    )
+    primary_email = models.ForeignKey(
+        EmailToContractor,
+        verbose_name=_("эл. адрес"),
+        related_name="+",
+        blank=True,
+        null=True,
+        help_text=_("перед тем как задать главный email добавьте его и перезагрузите страницу.")
+    )
+    addresses = models.ManyToManyField(
+        Address,
+        verbose_name=_("адреса"),
+        related_name="+"
+    )
+    primary_address = models.ForeignKey(
+        Address,
+        verbose_name=_("адрес"),
+        related_name="+",
+        blank=True,
+        null=True,
+        help_text=_("перед тем как задать главный адрес добавьте его и перезагрузите страницу.")
+    )
+    kind = models.CharField(
+        max_length=24,
+        null=True,
+        blank=True,
+        verbose_name=_("тип контрагента")
+    )
+    sector = models.CharField(
+        max_length=24,
+        null=True,
+        blank=True,
+        verbose_name=_("отрасль")
+    )
+    annual_income = models.BigIntegerField(
+        null=True,
+        blank=True,
+        verbose_name=_("годовой доход")
+    )
+    annual_income_currency = models.CharField(
+        max_length=8,
+        blank=True,
+        null=True,
+        verbose_name=_("валюта")
+    )
+    employee_num = models.IntegerField(
+        blank=True,
+        null=True,
+        verbose_name=_("число сотрудников")
+    )
+    sic = models.CharField(
+        max_length=128,
+        null=True,
+        blank=True,
+        verbose_name=_("ОКВЭД/SIC")
+    )
+    stock_code = models.CharField(
+        max_length=128,
+        null=True,
+        blank=True,
+        verbose_name=_("биржевой код")
+    )
+    own = models.CharField(
+        max_length=128,
+        blank=True,
+        null=True,
+        verbose_name=_("собственность")
+    )
+    parent = models.ForeignKey(
+        'self',
+        null=True,
+        blank=True,
+        verbose_name=_("состоит в")
+    )
+    # TODO marketing company
+    rating = models.CharField(
+        max_length=128,
+        blank=True,
+        null=True,
+        verbose_name=_("рейтинг")
+    )
+    responsible = models.ForeignKey(
+        User,
+        blank=True,
+        null=True,
+        verbose_name=_("Ответственный")
+    )
+
     def __str__(self):
         return self.name
+
+    class Meta:
+        verbose_name = _("контрагент")
+        verbose_name_plural = _("контрагенты")
